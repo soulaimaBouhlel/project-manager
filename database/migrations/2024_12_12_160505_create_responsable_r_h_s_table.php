12734e22a9de33b0eb9ccbf54b3d0cd67a1cdfa7
salsal123 <soulaimabouhlel2003@gmail.com>
2024-12-12 17:08:55 +0100

    first commit

---
+++ database/migrations/2024_12_12_160505_create_responsable_r_h_s_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responsable_r_h_s', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responsable_r_h_s');
+    }
+};
