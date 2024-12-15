12073f808de0fd9d8a9d4a86018c65aa2475f113
salsal123 <soulaimabouhlel2003@gmail.com>
2024-12-15 23:21:25 +0100

    add project features

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\EquipementController;
 
+Route::post('projects', [ProjetController::class, 'store']);
 Route::post('equipments', [EquipementController::class, 'store']);
 Route::post('/users', [UserController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
